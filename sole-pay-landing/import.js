a1f41aecd0fd7614cc94def4e0f00cc2db613342
kumarnitesh2000 <kumarnitesh2000.nk@gmail.com>
2021-05-27 12:06:48 +0530

    adding SUB_DIR_REDIRECT in .env land

--- sole-pay-landing/import.js
+++ sole-pay-landing/import.js
@@ -1,7 +1,7 @@
 import process from "process"
 console.log(process.env.FRONTEND_URL);
 let node = document.getElementById("redirect");
-node.href = process.env.FRONTEND_URL+"/build";
+node.href = process.env.FRONTEND_URL+process.env.SUB_DIR_REDIRECT;
 
 fetch(process.env.BACKEND_URL+'/api/pay/totalUpiCards')
 .then(res => res.json())
